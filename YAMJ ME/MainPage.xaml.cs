ea363fd80fa261d29a0a215941d336cc23fd5c3a
Haim Shalev <haimshalev@gmail.com>
2013-07-26 23:41:26 +0300

    Commit only the necessey files without all the project junk

---
+++ YAMJ ME/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+
+namespace YAMJ_ME
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //Run the YAMJ.cmd file
+        private void Run_YAMJ_Click(object sender, RoutedEventArgs e)
+        {
+            //Get the YAMJ path from configuration
+            var data = YAMJData.GetConfiguration();
+
+            //Start the process , if cannot show error message 
+            if (!StartProcess(data.YamjPath))
+            {
+                Run_YAMJ.Content = Constants.InvalidPathMessage;
+            }
+        }
+
+        //Run the YAMJ configuration tool
+        private void Run_YAMJ__Config_Click(object sender, RoutedEventArgs e)
+        {
+            //Get the YAMJ path from configuration
+            var data = YAMJData.GetConfiguration();
+
+            //Start the process , if cannot show error message 
+            if (!StartProcess(data.ConfigToolPath))
+            {
+                Run_Yamj_Config.Content = Constants.InvalidPathMessage;
+            }
+            
+        }
+
+        //Navigate to the settings page
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            //Create a new settings page
+            var settingsPage = new Settings();
+
+            //Navigate to the settings page using the navigation service
+            NavigationService navigationService = NavigationService;
+            if (navigationService != null) navigationService.Navigate(settingsPage);
+        }
+
+        //Try to start a process
+        private bool StartProcess(string path)
+        {
+            //if the config tool path initialized
+            if (path != null)
+            {
+                //Run the Yamj cmd process which creates the video library in the configured location
+                Process.Start(path);
+                return true;
+            }
+
+            //invalid path 
+            return false;
+        }
+    }
+}
---
+++ YAMJ ME/YAMJMeData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace YAMJ_ME
+{
+    static class Constants
+    {
+        public const string ConfigurationFile = "config.xml";
+        public const string InvalidPathMessage = "Invalid Path";
+
+    }
+
+    [Serializable]
+    public class YAMJData
+    {
+        public String YamjPath { get; set; }
+        public String ConfigToolPath { get; set; }
+
+        public static YAMJData GetConfiguration()
+        {
+            //Create a new data object
+            var data = new YAMJData();
+            var reader = new XmlSerializer(typeof(YAMJData));
+
+            //Read the configuration
+            try
+            {
+                using (FileStream input = File.OpenRead(Constants.ConfigurationFile))
+                {
+                    data = reader.Deserialize(input) as YAMJData;
+                }
+            }
+            catch (Exception)
+            {
+                //The file is in valid or damaged, lets create one from scretch
+                if (data != null) data.SetConfiguration();
+            }
+
+            return data;
+        }
+
+        public bool SetConfiguration()
+        {
+            // Write to XML
+            var writer = new XmlSerializer(typeof(YAMJData));
+            using (FileStream file = File.OpenWrite(Constants.ConfigurationFile))
+            {
+                writer.Serialize(file, this);
+            }
+
+            return true;
+        }
+
+    }
+}
